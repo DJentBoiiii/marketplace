f40d604e0b823a2c851a718bea6b66275c0857c2
Oleg_Makoid <oleh_makoid3007@tntu.edu.ua>
2025-03-13 11:27:12 +0000

    Remove unnecessary handlers

--- internal/handlers/mainHandlers.go
+++ internal/handlers/mainHandlers.go
@@ -7,15 +7,10 @@ import (
 )
 
 func index(c *fiber.Ctx) error {
-	return internal.RenderTemplate(c, "base.html", nil)
-}
-
-func gay(c *fiber.Ctx) error {
 	return internal.RenderTemplate(c, "index.html", nil)
 }
 
 func SetupHandlers(app *fiber.App) {
 	app.Get("/", index)
-	app.Get("/gay", gay)
 	handlers.SetupAuthHandlers(app)
 }
